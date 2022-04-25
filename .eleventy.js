7a6a0ace62a2c30d4fda5daaffc9418612a444d5
Daniel Zenzes <daniel@zenzes.me>
2022-04-25 21:29:17 +0200

    added tailwindcss

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,6 @@
 const eleventyPluginCookLang = require("eleventy-plugin-cooklang");
 const pluginNavigation = require("@11ty/eleventy-navigation");
-
+const now = String(Date.now());
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(pluginNavigation);
   eleventyConfig.addPlugin(eleventyPluginCookLang, {
@@ -52,6 +52,31 @@ module.exports = function (eleventyConfig) {
       }
     }
   );
+
+  eleventyConfig.addWatchTarget("./styles/tailwind.config.js");
+  eleventyConfig.addWatchTarget("./styles/tailwind.css");
+
+  eleventyConfig.addPassthroughCopy({ "./_tmp/style.css": "./style.css" });
+
+  eleventyConfig.addShortcode("version", function () {
+    return now;
+  });
+  eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
+    if (
+      process.env.ELEVENTY_PRODUCTION &&
+      outputPath &&
+      outputPath.endsWith(".html")
+    ) {
+      let minified = htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true,
+      });
+      return minified;
+    }
+
+    return content;
+  });
   return {
     dir: {
       input: "src",
